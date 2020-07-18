3af4d311c0b3e011c493e69c14232bbf0d487a19
Andrea Fiore <andrea.giulio.fiore@gmail.com>
2020-07-18 01:14:18 +0100

    render data in a simle table

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import humanizeDuration from 'humanize-duration';
 
+interface FailedTestcase {
+  name: string;
+  time: number;
+}
 
-class App extends React.Component<any, any> {
+interface FailedTestcases {
+  testcases: FailedTestcase[];
+}
+
+interface FailedTestSuite {
+  name: string;
+  time: number;
+  tests: number;
+  failures: number;
+  timestamp: Date;
+  failedTestcases: FailedTestcase[];
+}
+
+interface FailedSuites {
+  failed: FailedTestSuite[];
+}
+
+const FailedTestsFragment = ({ testcases }: FailedTestcases) => (
+  <React.Fragment>
+    {
+      testcases.map(test => {
+        return (<tr key={test.name}>
+          <td colSpan={3}>{test.name}</td>
+          <td>{humanizeDuration(test.time, { units: ["s", "ms"] })}</td>
+        </tr>)
+      })
+    }
+  </React.Fragment>
+)
+
+class App extends React.Component<any, FailedSuites> {
   constructor(props: any) {
     super(props);
+
     this.state = {
       failed: [],
     };
   }
   componentDidMount() {
-    fetch('http://localhost:3000/data.json')
+    fetch('/data.json')
       .then(response => response.json())
       .then(result => this.setState({
         failed: result.failed
@@ -21,15 +56,30 @@ class App extends React.Component<any, any> {
   render() {
     console.info(this.state);
     return (
-      <div>
-        <ul>
-          {this.state.failed.map((failure: any) => {
-            return (
-              <li>{failure.name}</li>
-            )
-          })}
-        </ul>
-      </div>
+      <table className="pure-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Tests</th>
+            <th>Failed</th>
+            <th>Duration</th>
+          </tr>
+        </thead>
+        {this.state.failed.map(suite => {
+          return (
+            <tbody>
+              <tr key={suite.name} className={"pure-table-odd"}>
+                <td>{suite.name}</td>
+                <td>{suite.tests}</td>
+                <td>{suite.failures}</td>
+                <td>{humanizeDuration(suite.time, { units: ["s", "ms"] })}</td>
+              </tr>
+              <FailedTestsFragment testcases={suite.failedTestcases} />
+            </tbody>
+          )
+        })
+        }
+      </table >
     );
   }
 }
